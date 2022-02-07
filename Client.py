d6a78aa20c021d827564bc6aaa731867f5b3cddc
Ohad Shirazi <92723105+ohad1s@users.noreply.github.com>
2022-02-07 17:13:51 +0200

    simple_chat

--- Client.py
+++ Client.py
@@ -3,57 +3,101 @@ import threading
 import tkinter
 import tkinter.scrolledtext
 from tkinter import simpledialog
-
-
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+# import tqdm
+# SEPARATOR = "<SEPARATOR>"
+# BUFFER_SIZE = 4096 # send 4096 bytes each time step
 
 class Client:
 
+
     def __init__(self):
+        """
+        this method is the constructor of the client chat
+        """
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect(('127.0.0.1', 55000))
 
         msg = tkinter.Tk()
         msg.withdraw()
 
-        self.nickname = simpledialog.askstring("NickName", "please enter your nickname", parent=msg)
 
+        self.nickname = simpledialog.askstring("NickName", "please enter your nickname", parent=msg)
         self.gui_play = False
         self.running = True
+        self.users=[]
+
 
         gui_thread = threading.Thread(target=self.gui_loop)
         receive_thread = threading.Thread(target=self.receive)
         gui_thread.start()
         receive_thread.start()
 
+
     def gui_loop(self):
+        """
+        this method is the gui loop for the chat
+        """
         self.win = tkinter.Tk()
         self.win.title("Ciiii Chat")
         self.win.configure(bg="lightgray")
 
-        self.chat_lable = tkinter.Label(self.win, text="Chat:", bg="lightgray")
-        self.chat_lable.config(font=('Helvetica', 12))
+        self.Frame3txt = Frame(master=self.win)
+        self.Frame3txt.grid(row = 0, column = 1, rowspan = 5, columnspan = 3, \
+                            sticky = W+E+N+S)
+        self.Frame4msg = Frame(master=self.win)
+        self.Frame4msg.grid(row = 5, column = 1, rowspan = 1, columnspan = 3, \
+                            sticky = W+E+N+S)
+
+        # self.Frame2files = Frame(master=self.win)
+        # self.Frame2files.grid(row = 3, column = 0, rowspan = 3, columnspan = 1, \
+        #                     sticky = W+E+N+S)
+        #
+        # self.send_file = tkinter.Button(self.Frame2files, text="Choose File", command=self.choose_file)
+        # self.send_file.config(font=('Ariel', 12))
+        # self.send_file.pack(padx=20, pady=5)
+
+        self.chat_lable = tkinter.Label(self.Frame3txt, text="Chat:", bg="lightgray")
+        self.chat_lable.config(font=('Ariel', 12))
         self.chat_lable.pack(padx=20, pady=5)
 
-        self.txt_area = tkinter.scrolledtext.ScrolledText(self.win)
+        self.txt_area = tkinter.scrolledtext.ScrolledText(self.Frame3txt)
         self.txt_area.pack(padx=20, pady=5)
         self.txt_area.config(state="disabled")
 
-        self.msg_lable = tkinter.Label(self.win, text="Message", bg="lightgray")
-        self.msg_lable.config(font=('Helvetica', 12))
+        self.msg_lable = tkinter.Label(self.Frame4msg, text="Message", bg="lightgray")
+        self.msg_lable.config(font=('Ariel', 12))
         self.msg_lable.pack(padx=20, pady=5)
 
-        self.msg_box = tkinter.Text(self.win, height=8)
+        self.msg_box = tkinter.Text(self.Frame4msg, height=8)
         self.msg_box.pack(padx=20, pady=5)
 
-        self.send_but = tkinter.Button(self.win, text="Send", command=self.write)
-        self.send_but.config(font=('Helvetica', 12))
+        self.send_but = tkinter.Button(self.Frame4msg, text="Send", command=self.write)
+        self.send_but.config(font=('Ariel', 12))
         self.send_but.pack(padx=20, pady=5)
+
+        self.nicknamesFrame1 = Frame(master=self.win)
+        self.nicknamesFrame1.grid(row = 0, column = 0, rowspan = 3, columnspan = 1, sticky = W+E+N+S)
+        self.user_list=Listbox(master=self.nicknamesFrame1)
+        self.user_list.pack(side="left",expand=1,fill="both")
+        self.userlist_scrollbar = Scrollbar(self.nicknamesFrame1,orient="vertical")
+        self.userlist_scrollbar.config(command=self.user_list.yview)
+        self.userlist_scrollbar.pack(side="left",fill="both")
+        self.user_list.config(yscrollcommand=self.userlist_scrollbar.set)
+
+        for nick in self.users:
+            self.user_list.insert(END,nick)
 
         self.gui_play = True
         self.win.protocol("WM_DELETE_WINDOW",self.stop)
         self.win.mainloop()
 
     def stop(self):
+        """
+        this method close the chat for a client
+        and also close the socket of the client
+        """
         self.running = False
         self.win.destroy()
         self.sock.close()
@@ -61,11 +105,18 @@ class Client:
 
 
     def receive(self):
+        """
+        this method handle the incoming messages in the client gui
+        """
         while self.running:
             try:
                 message = self.sock.recv(1024).decode('utf-8')
                 if message == 'NICKNAME':
                     self.sock.send(self.nickname.encode('utf-8'))
+                elif message.startswith("accounts"):
+                    self.users = message.split("|")
+                    self.users.remove("accounts")
+
                 else:
                     if (self.gui_play):
                         self.txt_area.config(state='normal')
@@ -78,8 +129,21 @@ class Client:
                 break
 
     def write(self):
+        """
+        this method handle the outgoing messages in the client gui
+        """
         message = f"{self.nickname}: {self.msg_box.get('1.0', 'end')}"
         self.sock.send(message.encode('utf-8'))
         self.msg_box.delete('1.0','end')
 
+    # def choose_file(self):
+    #     """
+    #
+    #     """
+    #     filename = askopenfilename()
+    #     self.sock.send(filename.encode('utf-8'))
+    #     self.msg_box.delete('1.0', 'end')
+
+
 client=Client()
+
--- Server.py
+++ Server.py
@@ -1,12 +1,15 @@
 import socket
 import threading
+import tkinter
+from tkinter import *
+
+nicknames_list=[]
+clients_list=[]
 
 host = '127.0.0.1'
 port = 55000
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    # IPV4 -TCP
 server.bind((host, port))
-clients = []
-nicknames = []
 
 def startChat():
     """
@@ -20,22 +23,34 @@ def startChat():
         print("Connected with {}".format(str(address)))
         client.send('NICKNAME'.encode("utf-8"))
         nickname = client.recv(1024).decode("utf-8")
-        nicknames.append(nickname)
-        clients.append(client)
+        nicknames_list.append(nickname)
+        clients_list.append(client)
         print("Nickname is {}".format(nickname))
+        client.send("Connected to server!\n".encode("utf-8"))
         send_msg("{} joined!\n".format(nickname).encode("utf-8"))
-        client.send('Connected to server!\n'.encode("utf-8"))
+        send_msg_to_someone("Connected to server!\n".encode("utf-8"),nickname)
+
+
         thread = threading.Thread(target=send, args=(client,))
         thread.start()
+
 
 def send_msg(message):
     """
     :param: message
     this method broadcast the message of each client to the chat
     """
-    for client in clients:
+    for client in clients_list:
         client.send(message)
-        server.sendto()
+
+def send_msg_to_someone(message,someone):
+    """
+    :param: message
+    this method broadcast the message of a specific client to other client
+    """
+    nickname_index=nicknames_list.index(someone)
+    client_to_send=clients_list[nickname_index]
+    client_to_send.send(message)
 
 def send(client):
     """
@@ -44,15 +59,25 @@ def send(client):
     """
     while True:
         try:
-            message = client.recv(1024)
-            send_msg(message)
+            data = "accounts|" + "|".join(nicknames_list)
+            send_msg(data.encode("utf-8"))
+            message = client.recv(1024).decode("utf-8")
+            msg_arr = message.split(" ")
+            if msg_arr[1]=="to":
+                someone=msg_arr[2]
+                if someone in nicknames_list:
+                    send_msg_to_someone(message.encode("utf-8"),someone)
+                else:
+                    send_msg(message.encode("utf-8"))
+            else:
+                send_msg(message.encode("utf-8"))
         except:
-            client_index = clients.index(client)
-            clients.remove(client)
+            client_index = clients_list.index(client)
+            clients_list.remove(client)
             client.close()
-            nickname = nicknames[client_index]
+            nickname = nicknames_list[client_index]
             send_msg('{} left!\n'.format(nickname).encode("utf-8"))
-            nicknames.remove(nickname)
+            nicknames_list.remove(nickname)
             break
 
 startChat()
